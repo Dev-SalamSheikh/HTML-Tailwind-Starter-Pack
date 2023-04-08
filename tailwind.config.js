55fcc5eafb9a9803ea0260b1a9245bfc93575d54
Salam Sheikh <dev.salamsheikh@gmail.com>
2023-04-08 19:41:32 +0600

    tailwind starter pack initialized

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  // no need this from here this are for example  , just need to add this path
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
